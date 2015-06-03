8c0864978ba48200e9eac244747f03343526e56a
Tigerkev <tigerkev@web.de>
2015-06-03 06:58:54 -0700

    Lua fixes.

--- lua/sh_colorx.lua
+++ lua/sh_colorx.lua
@@ -1,4 +1,4 @@
-/*
+--[[
 	 _____           _            
 	/  __ \         | |           
 	| /  \/_   _ ___| |_ ___  ___ 
@@ -9,14 +9,14 @@
 	~https://github.com/BadWolfGames/custos
 
 	Color X Library - Simple color conversion library.
-*/
+--]]
 colorx = {}
 local type = type
 local fmod, floor = math.fmod, math.floor
-local lshift, rshift, band = bit.lshift, bit.rshift, bit.band
+local lshift, rshift, band = bit32.lshift, bit32.rshift, bit32.band
 local conversionTable = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}
 
-//Converts Number to Hex string representation.
+--Converts Number to Hex string representation.
 function colorx.hextostring(hex)
 	local str = ""
 	while hex > 0 do
@@ -28,12 +28,12 @@ function colorx.hextostring(hex)
 	return str
 end
 
-//Unpack a color table.
+--Unpack a color table.
 function colorx.unpackcolor(tbl)
 	return tbl.r, tbl.g, tbl.b, tbl.a
 end
 
-//Convert RGB to Hex (0x000000)
+--Convert RGB to Hex (0x000000)
 function colorx.rgbtohex(r, g, b)
 	if type(r) == "table" then 
 		return colorx.rgbtohex(colorx.unpackcolor(r))
@@ -41,7 +41,7 @@ function colorx.rgbtohex(r, g, b)
 	return lshift(r, 16) + lshift(g, 8) + lshift(b, 0)
 end
 
-//Convert RGBA to Hex (0x00000000)
+--Convert RGBA to Hex (0x00000000)
 function colorx.rgbatohex(r, g, b, a)
 	if type(r) == "table" then
 		return colorx.rgbatohex(colorx.unpackcolor(r))
@@ -49,7 +49,7 @@ function colorx.rgbatohex(r, g, b, a)
 	return lshift(r, 24) + lshift(g, 16) + lshift(b, 8) + lshift(a, 0)
 end
 
-//Convert Hex to RGB
+--Convert Hex to RGB
 function colorx.hextorgb(hex)
 	local b = band(rshift(hex, 0), 255)
 	local g = band(rshift(hex, 8), 255)
@@ -58,7 +58,7 @@ function colorx.hextorgb(hex)
 	return r, g, b
 end
 
-//Convert Hex to RGBA
+--Convert Hex to RGBA
 function colorx.hextorgba(hex)
 	local a = band(rshift(hex, 0), 255)
 	local b = band(rshift(hex, 8), 255)
